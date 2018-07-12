08d01e05ce3246c67e04078e0a1dde12ca401c47
fangchungao <fangchungao@rltx.com>
2018-07-12 11:11:49 +0800

    1.ok

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vue-keep-live
+use
+1. npm install
+2. npm run serve
+3. open http://localhost:8080/
+
+this is only test tab
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="用户管理" name="first">
+              <HelloWorld msg="Welcome to Your Vue.js App" />
+            </el-tab-pane>
+            <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+            <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+            <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            activeName: 'second'
+        }
+    },
+    components: {
+        HelloWorld
+    },
+    methods: {
+        handleClick() {
+            // console.log(tab, event);
+        }
+    }
+}
+</script>
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
